9a6842265e89ccdf56e413383b0a235f33afd623
ArtemZagorovskYy <a.zagorovski23@gmail.com>
2020-07-15 10:48:25 +0300

    feat: model for news created

--- NewsApp/NewsApp/MVC/Model/News.swift
+++ NewsApp/NewsApp/MVC/Model/News.swift
@@ -7,3 +7,46 @@
 //
 
 import Foundation
+
+final class News {
+    
+    var newsTitle: String
+    var newsDescription: String
+    var imageData: Data?
+    var publishedAt: String?
+    
+    init(newsTitle: String, newsDescription: String, imageData: Data?, publishedAt: String?) {
+        
+        self.newsTitle = newsTitle
+        self.newsDescription = newsDescription
+        self.imageData = imageData
+        self.publishedAt = publishedAt
+    }
+}
+
+extension News: JSONDecodable {
+    convenience init?(JSON: [String : AnyObject]) {
+        guard let title = JSON["title"] as? String,
+            let description = JSON["description"] as? String,
+            let publishedAt = JSON["publishedAt"] as? String else {
+                return nil
+        }
+        
+        var imageData: Data? = nil
+        do {
+            let imageUrlString = JSON["urlToImage"]
+            if imageUrlString is NSNull {
+                imageData = nil
+            } else {
+                if let imageUrl = URL(string: imageUrlString as! String) {
+                    imageData = try Data(contentsOf: imageUrl)
+                } else {
+                    print("\(imageUrlString) is not an image")
+                }
+            }
+        } catch let error {
+            print("error \(error.localizedDescription)")
+        }
+        self.init(newsTitle: title, newsDescription: description, imageData: imageData, publishedAt: publishedAt)
+    }
+}

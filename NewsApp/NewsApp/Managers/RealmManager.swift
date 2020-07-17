6f02e25cbb2d953f4dd0ea848ea3160394f5cb95
ArtemZagorovskYy <a.zagorovski23@gmail.com>
2020-07-17 11:40:12 +0300

    fix: change name

---
+++ NewsApp/NewsApp/Managers/RealmManager.swift
@@ -0,0 +1,27 @@
+//
+//  StorageManager.swift
+//  NewsApp
+//
+//  Created by Артем  on 7/15/20.
+//  Copyright © 2020 Artem Zagorovski. All rights reserved.
+//
+
+import RealmSwift
+
+let realm = try! Realm()
+
+class RealmManager {
+    static func saveNews(_ news: News) {
+        
+        try! realm.write {
+            realm.add(news)
+        }
+    }
+    
+    static func deliteNews (_ news: News) {
+        try! realm.write {
+            realm.delete(news)
+        }
+    }
+    
+}

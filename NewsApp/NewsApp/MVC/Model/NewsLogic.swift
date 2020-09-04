8ddc8085a89ace84f1836bce917ddd4000763fa2
Zagorovsky <zagorovskyan@QWS-ZAG-MAC.local>
2020-09-04 12:19:46 +0300

    add file for News logic

---
+++ NewsApp/NewsApp/MVC/Model/NewsLogic.swift
@@ -0,0 +1,13 @@
+//
+//  NewsLogic.swift
+//  NewsApp
+//
+//  Created by Zagorovsky, Artem on 9/2/20.
+//  Copyright © 2020 Artem Zagorovski. All rights reserved.
+//
+
+import Foundation
+
+final class NewsLogic {
+    
+}

54f8af3aa9f2ac32b0555aa855607e6eaaa29511
Zagorovsky <zagorovskyan@QWS-ZAG-MAC.local>
2020-09-07 17:55:02 +0300

    Task-3: converting from News model to viewModel

--- NewsApp/NewsApp/Services/Coordinator.swift
+++ NewsApp/NewsApp/Services/Coordinator.swift
@@ -6,7 +6,7 @@
 //  Copyright © 2020 Artem Zagorovski. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 class Coordinator: ViewDelegate, ModelDelegate {
     
@@ -34,12 +34,22 @@ class Coordinator: ViewDelegate, ModelDelegate {
         model.filter(for: term)
     }
     
-    func viewDidTapFavouriteButton(for viewModel: ViewModel) {
+    func viewDidTapFavouriteButton(for viewModel: [ViewModel]) {
         model.updateFavourite()
     }
     
     func modelDidLoadNews(_ news: [News]) {
-        view.updateView(news)
+        var viewModels = [ViewModel]()
+        news.forEach{ news in
+            guard let imageData = news.imageData, let image = UIImage(data: imageData) else { return }
+            let viewModel = ViewModel(newsTitle: news.newsTitle,
+                                      newsDescription: news.newsDescription,
+                                      image: image,
+                                      publishedAt: news.publishedAt,
+                                      isFavourite: news.isFavourite)
+            viewModels.append(viewModel)
+        }
+        view.updateView(viewModels)
     }
     
 }

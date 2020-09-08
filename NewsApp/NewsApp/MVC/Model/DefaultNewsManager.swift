a8776c068ca3851e9d551f9e2841f8d89ba1db90
Zagorovsky <zagorovskyan@QWS-ZAG-MAC.local>
2020-09-08 19:45:04 +0300

    Task-3: protocols go to extensions

--- NewsApp/NewsApp/MVC/Model/DefaultNewsManager.swift
+++ NewsApp/NewsApp/MVC/Model/DefaultNewsManager.swift
@@ -8,52 +8,72 @@
 
 import Foundation
 
-final class DefaultNewsManager: NewsManager, DataManagerDelegate {
+final class DefaultNewsManager {
     
-    var serviceManager: ServiceManager?
-    weak var newsLogicDelegate: ModelDelegate?
+    private var serviceManager: ServiceManager?
+    weak var delegate: NewsManagerDelegate?
     
     private var news: [News]?
+    private var searchNews: [News]?
+    private var countOfDays = 0
+    
+    init(serviceManager: ServiceManager) {
+        self.serviceManager = serviceManager
+    }
+    
+    func computeDate() -> String {
+        let date = Date().rewindDays(-countOfDays)
+        return Formatter.getStringWithWeekDay(date: date)
+    }
+    
+}
+
+extension DefaultNewsManager: NewsManager {
     
     func loadNews() {
-        computeData()
-        serviceManager?.getData()
-        Constants.Logic.countOfDays += 1
+        serviceManager?.getData(date: computeDate())
+        countOfDays += 1
     }
     
     func filter(for text: String) {
-        let searchNews = news?.filter { news in
-            let isTitleContainsFilter = news.newsTitle.lowercased().contains(text.lowercased())
-            let isDescriptionContainsFilter = news.newsDescription.lowercased().contains(text.lowercased())
-            return text.isEmpty ? true : isTitleContainsFilter || isDescriptionContainsFilter
+        if text.isEmpty {
+            searchNews = news
+        } else {
+            searchNews = news?.filter { news in
+                let isTitleContainsFilter = news.newsTitle.lowercased().contains(text.lowercased())
+                let isDescriptionContainsFilter = news.newsDescription.lowercased().contains(text.lowercased())
+                return isTitleContainsFilter || isDescriptionContainsFilter
+            }
         }
-        newsLogicDelegate?.modelDidLoadNews(searchNews!)
+        guard let searchNews = searchNews else { return }
+        delegate?.modelDidLoadNews(searchNews)
     }
     
     func refresh() {
-        Constants.Logic.countOfDays = 0
-        serviceManager?.getData()
+        countOfDays = 0
+        serviceManager?.getData(date: computeDate())
     }
     
     func loadMoreNews() {
-        computeData()
-        serviceManager?.getData()
-        Constants.Logic.countOfDays += 1
+        serviceManager?.getData(date: computeDate())
+        countOfDays += 1
     }
     
     func updateFavourite() {
         print("New favourite")
     }
     
+}
+
+extension DefaultNewsManager: ServiceManagerDelegate {
+    
     func dataManagerDidLoadData(_ news: [News]) {
         self.news = news
-        newsLogicDelegate?.modelDidLoadNews(news)
+        delegate?.modelDidLoadNews(news)
     }
     
-    func computeData() {
-        let date = Date().rewindDays(-Constants.Logic.countOfDays)
-        let dateString = Formatter.getStringWithWeekDay(date: date)
-        Constants.Api.currentDateString = dateString
+    func dataManagerDidGetAnError(error: Error) {
+        delegate?.modelDidGetAnError(error: error)
     }
     
 }

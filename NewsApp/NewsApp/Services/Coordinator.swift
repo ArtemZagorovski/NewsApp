9347fa0f51978c6d1a6045208444edcaf1e88c8e
Zagorovsky <zagorovskyan@QWS-ZAG-MAC.local>
2020-09-08 12:03:02 +0300

    Task-3: make delegates weak and clean up

--- NewsApp/NewsApp/Services/Coordinator.swift
+++ NewsApp/NewsApp/Services/Coordinator.swift
@@ -8,10 +8,10 @@
 
 import UIKit
 
-class Coordinator: ViewDelegate, ModelDelegate {
+final class Coordinator: ViewDelegate, ModelDelegate {
     
-    var model: NewsManager
-    var view: View
+    private var model: NewsManager
+    private var view: View
     
     init (model: NewsManager, view: View) {
         self.model = model
@@ -19,34 +19,35 @@ class Coordinator: ViewDelegate, ModelDelegate {
     }
     
     func viewDidLoad() {
+        view.animateActivity()
         model.loadNews()
     }
     
     func viewDidScrollTheEnd() {
-        model.loadNews()
+        model.loadMoreNews()
     }
     
     func viewDidPullToRefresh() {
-        model.loadNews()
+        model.refresh()
     }
     
     func viewDidChangeSearchTerm(_ term: String) {
         model.filter(for: term)
     }
     
-    func viewDidTapFavouriteButton(for viewModel: [ViewModel]) {
+    func viewDidTapFavouriteButton(for viewModel: [NewsViewModel]) {
         model.updateFavourite()
     }
     
     func modelDidLoadNews(_ news: [News]) {
-        var viewModels = [ViewModel]()
-        news.forEach{ news in
+        var viewModels: [NewsViewModel] = []
+        news.forEach { news in
             guard let imageData = news.imageData, let image = UIImage(data: imageData) else { return }
-            let viewModel = ViewModel(newsTitle: news.newsTitle,
-                                      newsDescription: news.newsDescription,
-                                      image: image,
-                                      publishedAt: news.publishedAt,
-                                      isFavourite: news.isFavourite)
+            let viewModel = NewsViewModel(newsTitle: news.newsTitle,
+                                          newsDescription: news.newsDescription,
+                                          image: image,
+                                          publishedAt: news.publishedAt,
+                                          isFavourite: news.isFavourite)
             viewModels.append(viewModel)
         }
         view.updateView(viewModels)

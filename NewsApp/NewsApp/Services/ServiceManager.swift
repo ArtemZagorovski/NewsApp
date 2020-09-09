2509e686b3e832ca7650c0698e096eebad614154
Zagorovsky <zagorovskyan@QWS-ZAG-MAC.local>
2020-09-09 12:38:42 +0300

    Task-5: add method for getting total number of news

--- NewsApp/NewsApp/Services/ServiceManager.swift
+++ NewsApp/NewsApp/Services/ServiceManager.swift
@@ -10,18 +10,20 @@ import Foundation
 
 protocol NewsServiceCoordinator {
     func getData(page: Int)
+    func getNewsTotalNumber()
 }
 
 protocol NewsServiceCoordinatorDelegate: class {
-    func dataManagerDidLoadData(_ news: [News])
-    func dataManagerDidGetAnError(error: Error)
+    func serviceManagerDidLoadData(_ news: [News])
+    func serviceManagerDidGetTotalNews(total: Int)
+    func serviceManagerDidGetAnError(error: Error)
 }
 
 final class ServiceManager: NewsServiceCoordinator {
     
     weak var delegate: NewsServiceCoordinatorDelegate?
-    private var apiService: RemoteNewsService?
-    private var dbService: LocalNewsService?
+    private var apiService: RemoteNewsService
+    private var dbService: LocalNewsService
     
     init(apiService: RemoteNewsService, dbService: LocalNewsService) {
         self.apiService = apiService
@@ -30,11 +32,15 @@ final class ServiceManager: NewsServiceCoordinator {
     
     func getData(page: Int) {
         if Constants.Logic.countOfDays > 0 {
-            apiService?.getData(page: page)
+            apiService.getData(page: page)
         } else {
-            dbService?.removeData()
-            apiService?.getData(page: page)
+            dbService.removeData()
+            apiService.getData(page: page)
         }
+    }
+    
+    func getNewsTotalNumber() {
+        apiService.getNewsTotalNumber()
     }
     
 }
@@ -42,14 +48,18 @@ final class ServiceManager: NewsServiceCoordinator {
 extension ServiceManager: NewsServiceDelegate {
     
     func didLoadData(_ news: [News]) {
-        delegate?.dataManagerDidLoadData(news)
+        delegate?.serviceManagerDidLoadData(news)
         DispatchQueue.main.async {
-            self.dbService?.saveData(news)
+            self.dbService.saveData(news)
         }
     }
     
+    func didGetTotalNews(total: Int) {
+        delegate?.serviceManagerDidGetTotalNews(total: total)
+    }
+    
     func didGetAnError(error: Error) {
-        delegate?.dataManagerDidGetAnError(error: error)
+        delegate?.serviceManagerDidGetAnError(error: error)
     }
     
 }

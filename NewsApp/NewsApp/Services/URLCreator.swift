c52515f36dd2824b86437fc4ffcccb61998ea46a
Zagorovsky <zagorovskyan@QWS-ZAG-MAC.local>
2020-09-10 20:02:39 +0300

    Task-7: add logic

--- NewsApp/NewsApp/Services/URLCreator.swift
+++ NewsApp/NewsApp/Services/URLCreator.swift
@@ -7,3 +7,28 @@
 //
 
 import Foundation
+
+final class URLCreator {
+    private let scheme = "http"
+    private let host = "newsapi.org"
+    private let path = "/v2/everything"
+    private let q = "Apple"
+    private let sortBy = "popular"
+    private let language = "en"
+    private let apiKey = "6a2f2df98b7d4086a3aa0b9877d333a9"
+    
+    func createUrl(with page: Int) -> URL? {
+        var components = URLComponents()
+        components.scheme = scheme
+        components.host = host
+        components.path = path
+        components.queryItems = [
+            URLQueryItem(name: "q", value: q),
+            URLQueryItem(name: "sortBy", value: sortBy),
+            URLQueryItem(name: "language", value: language),
+            URLQueryItem(name: "page", value: String(page)),
+            URLQueryItem(name: "apiKey", value: apiKey)
+        ]
+        return components.url
+    }
+}

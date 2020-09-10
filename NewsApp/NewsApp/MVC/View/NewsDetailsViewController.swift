2d3c7064603e9c8127e0a0bbff2f74a00a05623d
Zagorovsky <zagorovskyan@QWS-ZAG-MAC.local>
2020-09-10 15:06:46 +0300

    Task-6: return init

--- NewsApp/NewsApp/MVC/View/NewsDetailsViewController.swift
+++ NewsApp/NewsApp/MVC/View/NewsDetailsViewController.swift
@@ -24,6 +24,15 @@ class NewsDetailsViewController: UIViewController {
         setupLayout()
     }
     
+    init(news: NewsViewModel) {
+        super.init(nibName: nil, bundle: nil)
+        self.news = news
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
 }
 
 extension NewsDetailsViewController{

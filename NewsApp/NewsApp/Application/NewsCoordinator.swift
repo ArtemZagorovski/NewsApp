fe760f31ee1cb6c363e69ed49d0ff625993e9b08
Zagorovsky <zagorovskyan@QWS-ZAG-MAC.local>
2020-09-10 17:16:41 +0300

    Task-6: replace show error alert

--- NewsApp/NewsApp/Application/NewsCoordinator.swift
+++ NewsApp/NewsApp/Application/NewsCoordinator.swift
@@ -31,10 +31,17 @@ final class NewsCoordinator {
         return view
     }
     
-    func createNewsDetailsCoordinator(with: NewsViewModel) {
+    func showDetails(with viewModel: NewsViewModel) {
         let newsDetailsCoordinator = NewsDetailsCoordinator()
-        let newsDetailsViewController = newsDetailsCoordinator.createNewsDetailsViewController(with: with)
+        let newsDetailsViewController = newsDetailsCoordinator.createNewsDetailsViewController(with: viewModel)
         mainView?.navigationController?.pushViewController(newsDetailsViewController, animated: true)
+    }
+    
+    func showAnError(error: Error) {
+        let alert = UIAlertController(title: "Error", message: error.localizedDescription, preferredStyle: .alert)
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+        alert.addAction(cancelAction)
+        mainView?.present(alert, animated: true)
     }
     
 }

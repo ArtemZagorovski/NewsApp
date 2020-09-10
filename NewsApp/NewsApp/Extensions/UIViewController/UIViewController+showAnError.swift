72211b865e24215ca636d30722ac7332673a382d
Zagorovsky <zagorovskyan@QWS-ZAG-MAC.local>
2020-09-10 13:53:30 +0300

    Task-6: replace showAnErrorMethod in extension

--- NewsApp/NewsApp/Extensions/UIViewController/UIViewController+showAnError.swift
+++ NewsApp/NewsApp/Extensions/UIViewController/UIViewController+showAnError.swift
@@ -6,4 +6,15 @@
 //  Copyright © 2020 Artem Zagorovski. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+extension UIViewController {
+    
+    func showAnError(error: Error) {
+        let alert = UIAlertController(title: "Error", message: error.localizedDescription, preferredStyle: .alert)
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+        alert.addAction(cancelAction)
+        present(alert, animated: true)
+    }
+    
+}

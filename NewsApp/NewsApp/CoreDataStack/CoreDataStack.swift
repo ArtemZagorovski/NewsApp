52c07c9f5799c1a8f17e2cade91c6dd12d9461a1
Zagorovsky <zagorovskyan@QWS-ZAG-MAC.local>
2020-09-11 18:05:07 +0300

    Task-9: change name of xcdatamodel

--- NewsApp/NewsApp/CoreDataStack/CoreDataStack.swift
+++ NewsApp/NewsApp/CoreDataStack/CoreDataStack.swift
@@ -7,3 +7,16 @@
 //
 
 import Foundation
+import CoreData
+
+final class CoreDataStack {
+    lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "News")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+}

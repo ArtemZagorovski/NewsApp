a84ea9c7996ea6bbb888ec77072570ad1d24ba17
Zagorovsky <zagorovskyan@QWS-ZAG-MAC.local>
2020-09-14 18:07:36 +0300

    Task-11: add connecting logic

--- NewsApp/NewsApp/Application/FavouriteNewsContractProtocols.swift
+++ NewsApp/NewsApp/Application/FavouriteNewsContractProtocols.swift
@@ -7,3 +7,20 @@
 //
 
 import Foundation
+
+protocol FavouriteNewsViewDelegate: class {
+    func viewDidLoad()
+    func viewDidChangeSearchTerm(_ term: String)
+    func viewDidTapFavouriteButton(for viewModel: NewsViewModel)
+    func viewDidTapCell(for viewModel: NewsViewModel)
+}
+
+protocol FavouriteNewsView: class {
+    func updateView(_ news: [NewsViewModel])
+}
+
+protocol FavouriteNewsManager {
+    func loadNews()
+    func filter(for text: String)
+    func updateFavoutire()
+}

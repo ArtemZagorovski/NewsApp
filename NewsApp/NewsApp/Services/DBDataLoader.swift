0d07cc34fdafcf64bd95e651fcd50985b0b2e891
Zagorovsky <zagorovskyan@QWS-ZAG-MAC.local>
2020-09-14 13:24:44 +0300

    Task-9: logic with 2 different contexts

--- NewsApp/NewsApp/Services/DBDataLoader.swift
+++ NewsApp/NewsApp/Services/DBDataLoader.swift
@@ -7,34 +7,43 @@
 //
 
 import Foundation
-import RealmSwift
+import CoreData
 
 final class DBDataLoader: LocalNewsService {
-    
     private let context = CoreDataStack().persistentContainer.viewContext
+    private let getContext = NSManagedObjectContext(concurrencyType: .privateQueueConcurrencyType)
+    private let saveContext = NSManagedObjectContext(concurrencyType: .privateQueueConcurrencyType)
     weak var delegate: NewsServiceDelegate?
-
+    private var newsFromBD: [News] = []
+    
+    init() {
+        getContext.parent = context
+        saveContext.parent = context
+    }
+    
     func getData(page: Int) {
-        var news: [News] = []
         do {
-            guard let newsCD = try context.fetch(NewsCoreData.fetchRequest()) as? [NewsCoreData] else { return }
-            news = newsCD.map{News(newsCD: $0)}
+            guard let newsCD = try getContext.fetch(NewsEntity.fetchRequest()) as? [NewsEntity] else { return }
+            newsFromBD = newsCD.compactMap{News(newsCD: $0)}
         }
         catch let error {
             delegate?.didGetAnError(error: error)
         }
-        delegate?.didLoadData(news)
+        delegate?.didLoadData(newsFromBD)
     }
     
     func saveData(_ news: [News]) {
         news.forEach { news in
-            NewsCoreData(news: news, context: context)
+            getData(page: 1)
+            if !newsFromBD.contains(news) {
+                NewsEntity(news: news, context: saveContext)
+            }
         }
         do {
-            try context.save()
+            try saveContext.save()
         }
         catch let error {
-            print(error.localizedDescription)
+            delegate?.didGetAnError(error: error)
         }
     }
 }

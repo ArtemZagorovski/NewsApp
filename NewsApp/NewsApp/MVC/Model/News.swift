0fe4d1835bf4fba798899b9759a66de71a074638
Zagorovsky <zagorovskyan@QWS-ZAG-MAC.local>
2020-09-15 10:15:30 +0300

    Task-9: replace extensions

--- NewsApp/NewsApp/MVC/Model/News.swift
+++ NewsApp/NewsApp/MVC/Model/News.swift
@@ -15,54 +15,12 @@ final class News {
     let publishedAt: String?
     var isFavourite: Bool = false
     
-    private init (newsTitle: String, newsDescription: String, imageData: Data?, publishedAt: String?, isFavourite: Bool) {
+    init (newsTitle: String, newsDescription: String, imageData: Data?, publishedAt: String?, isFavourite: Bool) {
         self.newsTitle = newsTitle
         self.newsDescription = newsDescription
         self.imageData = imageData
         self.publishedAt = publishedAt
         self.isFavourite = isFavourite
-    }
-}
-
-extension News {
-    convenience init?(JSON: [String : AnyObject]) {
-        guard let title = JSON["title"] as? String,
-            let description = JSON["description"] as? String,
-            let publishedAt = JSON["publishedAt"] as? String else {
-                return nil
-        }
-        
-        var imageData: Data? = nil
-        do {
-            let imageUrlString = JSON["urlToImage"]
-            if imageUrlString is NSNull {
-                imageData = nil
-            } else {
-                if let imageUrl = URL(string: imageUrlString as! String) {
-                    imageData = try Data(contentsOf: imageUrl)
-                } else {
-                    print("imageUrlString is not an image")
-                }
-            }
-        } catch let error {
-            print("error \(error.localizedDescription)")
-        }
-
-        self.init(newsTitle: title,
-                  newsDescription: description,
-                  imageData: imageData,
-                  publishedAt: publishedAt,
-                  isFavourite: false)
-    }
-}
-
-extension News {
-    convenience init?(newsCD: NewsEntity) {
-        self.init(newsTitle: newsCD.newsTitle,
-                  newsDescription: newsCD.newsDescription,
-                  imageData: newsCD.imageData,
-                  publishedAt: newsCD.publishedAt,
-                  isFavourite: newsCD.isFavourite)
     }
 }
 

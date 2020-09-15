9e38db35bfb678d22e78d869e62b7519462e0377
Zagorovsky <zagorovskyan@QWS-ZAG-MAC.local>
2020-09-15 10:14:20 +0300

    Task-9: replace extension

--- NewsApp/NewsApp/Services/News+initFromAPI.swift
+++ NewsApp/NewsApp/Services/News+initFromAPI.swift
@@ -7,3 +7,35 @@
 //
 
 import Foundation
+
+extension News {
+    convenience init?(JSON: [String : AnyObject]) {
+        guard let title = JSON["title"] as? String,
+            let description = JSON["description"] as? String,
+            let publishedAt = JSON["publishedAt"] as? String else {
+                return nil
+        }
+        
+        var imageData: Data? = nil
+        do {
+            let imageUrlString = JSON["urlToImage"]
+            if imageUrlString is NSNull {
+                imageData = nil
+            } else {
+                if let imageUrlString = imageUrlString as? String, let imageUrl = URL(string: imageUrlString) {
+                    imageData = try Data(contentsOf: imageUrl)
+                } else {
+                    print("imageUrlString is not an image")
+                }
+            }
+        } catch let error {
+            print("error \(error.localizedDescription)")
+        }
+
+        self.init(newsTitle: title,
+                  newsDescription: description,
+                  imageData: imageData,
+                  publishedAt: publishedAt,
+                  isFavourite: false)
+    }
+}

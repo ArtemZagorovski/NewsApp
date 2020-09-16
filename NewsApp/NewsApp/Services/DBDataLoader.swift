bed723fa9f64dd7329bee9978dc1be1d483ef8d1
Zagorovsky <zagorovskyan@QWS-ZAG-MAC.local>
2020-09-16 16:34:26 +0300

    Task-12: crud logic

--- NewsApp/NewsApp/Services/DBDataLoader.swift
+++ NewsApp/NewsApp/Services/DBDataLoader.swift
@@ -10,11 +10,12 @@ import Foundation
 import CoreData
 
 final class DBDataLoader: LocalNewsService {
-    private let persistentContainer = CoreDataStack().persistentContainer
+    
+    private let persistentContainer = CoreDataStack.persistentContainer
     private let getContext: NSManagedObjectContext
     private let saveContext: NSManagedObjectContext
-    weak var delegate: NewsServiceDelegate?
-    private var newsFromBD: [News] = []
+    weak var delegate: NewsLocalServiceDelegate?
+    private var newsFromBD: [NewsEntity] = []
     
     init() {
         getContext = persistentContainer.viewContext
@@ -22,28 +23,52 @@ final class DBDataLoader: LocalNewsService {
     }
     
     func getData(page: Int) {
+        getCurrentState()
+        delegate?.didLoadData(newsFromBD)
+    }
+    
+    private func getCurrentState() {
         do {
             guard let newsCD = try getContext.fetch(NewsEntity.fetchRequest()) as? [NewsEntity] else { return }
-            newsFromBD = newsCD.compactMap { News(newsCD: $0) }
+            newsFromBD = newsCD
         }
         catch let error {
-            delegate?.didGetAnError(error: error)
+            print(error.localizedDescription)
+        }
+    }
+    
+    func filter(for text: String) {
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "NewsEntity")
+        if text.isEmpty {
+            return
+        }
+        let predicate = NSPredicate(format: "newsTitle CONTAINS[c] %@", text)
+        fetchRequest.predicate = predicate
+        do {
+            guard let fetchResult = try getContext.fetch(fetchRequest) as? [NewsEntity] else { return }
+            newsFromBD = fetchResult
+        }
+        catch let error {
+            print(error.localizedDescription)
         }
         delegate?.didLoadData(newsFromBD)
     }
     
-    func saveData(_ news: [News]) {
-        
-        news.forEach { news in
-            if !newsFromBD.contains(news) {
-                NewsEntity(news: news, context: saveContext)
-            }
+    func saveData(_ news: News, closure: () -> ()) {
+        getCurrentState()
+        let equals = newsFromBD.filter { $0.id == news.id }
+        if equals.isEmpty {
+            NewsEntity(news: news, context: saveContext)
+        } else {
+            equals.map { getContext.delete($0) }
+            getData(page: 1)
         }
         do {
             try saveContext.save()
+            closure()
         }
         catch let error {
-            delegate?.didGetAnError(error: error)
+            print(error.localizedDescription)
         }
     }
 }

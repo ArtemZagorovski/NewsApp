6a163c524ef8ec70b637ec0be1dc64c680f9f482
Zagorovsky <zagorovskyan@QWS-ZAG-MAC.local>
2020-09-17 15:32:30 +0300

    Task-15: create internet connection manager

---
+++ NewsApp/NewsApp/Helpers/InternetConnectionManager.swift
@@ -0,0 +1,9 @@
+//
+//  InternetConnectionManager.swift
+//  NewsApp
+//
+//  Created by Zagorovsky, Artem on 9/17/20.
+//  Copyright © 2020 Artem Zagorovski. All rights reserved.
+//
+
+import Foundation

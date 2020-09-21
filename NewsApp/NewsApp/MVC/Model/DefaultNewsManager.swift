9a0715c5444acc6f87186e700c7b239e75bd426b
Zagorovsky <zagorovskyan@QWS-ZAG-MAC.local>
2020-09-21 18:36:41 +0300

    Task-12: change filter logic

--- NewsApp/NewsApp/MVC/Model/DefaultNewsManager.swift
+++ NewsApp/NewsApp/MVC/Model/DefaultNewsManager.swift
@@ -9,17 +9,17 @@
 import Foundation
 
 final class DefaultNewsManager: NewsManager {
-    private let apiService: RemoteNewsService
-    private var dbService: LocalNewsService
+    private let apiService = APIService()
+    private let dbService = DBDataLoader()
     private var newsFromApi: [News] = []
     private var newsFromBD: [News] = []
     private var page = 1
     
     weak var delegate: NewsManagerDelegate?
     
-    init(apiService: RemoteNewsService, dbService: LocalNewsService) {
-        self.apiService = apiService
-        self.dbService = dbService
+    init() {
+        self.apiService.delegate = self
+        self.dbService.delegate = self
         dbService.loadNews(page: 1)
     }
     
@@ -27,11 +27,12 @@ final class DefaultNewsManager: NewsManager {
         apiService.loadNews(page: page)
     }
     
-    func filterAllNews(for text: String) {
+    func filter(favorite: Bool, for text: String) {
+        let news = favorite ? newsFromBD : newsFromApi
         if text.isEmpty {
-            delegate?.modelDidLoadNews(newsFromApi)
+            delegate?.modelDidLoadNews(news)
         } else {
-            delegate?.modelDidLoadNews(newsFromApi.filter { news in
+            delegate?.modelDidLoadNews(news.filter { news in
                 let isTitleContainsFilter = news.newsTitle.lowercased().contains(text.lowercased())
                 let isDescriptionContainsFilter = news.newsDescription.lowercased().contains(text.lowercased())
                 return isTitleContainsFilter || isDescriptionContainsFilter
@@ -53,15 +54,14 @@ final class DefaultNewsManager: NewsManager {
         dbService.saveData(newsFromBD)
     }
     
-    func updateFavorites(with news: News, refreshCell: @escaping () -> ()) {
-        let equals = newsFromBD.filter { $0.id == news.id }
-        if equals.isEmpty {
-            news.isFavourite = !news.isFavourite
-            newsFromBD.append(news)
+    func updateFavorites(with news: News, isFavorite: Bool, refreshCell: @escaping () -> ()) {
+        if isFavorite {
+            guard let indexOfEqual = newsFromBD.firstIndex(of: news) else { return }
+            newsFromBD.remove(at: indexOfEqual)
         }
         else {
-            guard let indexOfEqual = newsFromBD.firstIndex(of: equals[0]) else { return }
-            newsFromBD.remove(at: indexOfEqual)
+            news.isFavourite = !news.isFavourite
+            newsFromBD.append(news)
         }
         refreshCell()
     }
@@ -71,24 +71,12 @@ extension DefaultNewsManager: FavoriteNewsManager {
     func loadFavoriteNews() {
         delegate?.modelDidLoadNews(newsFromBD)
     }
-    
-    func filterFavoriteNews(for text: String) {
-        if text.isEmpty {
-            delegate?.modelDidLoadNews(newsFromBD)
-        } else {
-            delegate?.modelDidLoadNews(newsFromBD.filter { news in
-                let isTitleContainsFilter = news.newsTitle.lowercased().contains(text.lowercased())
-                let isDescriptionContainsFilter = news.newsDescription.lowercased().contains(text.lowercased())
-                return isTitleContainsFilter || isDescriptionContainsFilter
-            })
-        }
-    }
 }
 
 extension DefaultNewsManager: NewsRemoteServiceDelegate {
     func didLoadData(_ news: [[String : AnyObject]]) {
         newsFromApi = news.compactMap { News(JSON: $0) }
-        newsFromApi.filter { newsFromBD.contains($0) }.map { $0.isFavourite = true }
+        newsFromApi.forEach { $0.isFavourite = newsFromBD.contains($0) }
         delegate?.modelDidLoadNews(newsFromApi)
     }
     
@@ -100,6 +88,6 @@ extension DefaultNewsManager: NewsRemoteServiceDelegate {
 extension DefaultNewsManager: NewsLocalServiceDelegate {
     func didLoadData(_ news: [NewsEntity]) {
         newsFromBD = news.compactMap { News(newsCD: $0) }
-        newsFromBD.map {$0.isFavourite = true}
+        newsFromBD.forEach {$0.isFavourite = true}
     }
 }

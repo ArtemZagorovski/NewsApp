2837f103c244537c7966ae49b7e3bce1c827e9d9
Zagorovsky <zagorovskyan@QWS-ZAG-MAC.local>
2020-09-23 16:03:08 +0300

    Task-12: add enum with errors

---
+++ NewsApp/NewsApp/Services/NewsError.swift
@@ -0,0 +1,13 @@
+//
+//  NewsError.swift
+//  NewsApp
+//
+//  Created by Zagorovsky, Artem on 9/23/20.
+//  Copyright © 2020 Artem Zagorovski. All rights reserved.
+//
+
+import Foundation
+
+enum NewsError: Error {
+    case parseDataError
+}

280e7c7f483724be151f4b5f970e2e2c0373d13e
ArtemZagorovski <artemzagorovski@mail.ru>
2020-09-23 15:24:58 +0300

    Bugfix/task 12: change logic of refresh cell closure (#45)
    * Task-12: change logic of refresh cell closure
    
    * Task-12: rename parameters
    
    * Task-12: remove escaping
    
    * Task-12: create parent protocol
    
    Co-authored-by: Zagorovsky <zagorovskyan@QWS-ZAG-MAC.local>

--- NewsApp/NewsApp/MVC/Model/DefaultNewsManager.swift
+++ NewsApp/NewsApp/MVC/Model/DefaultNewsManager.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-final class DefaultNewsManager: NewsManager {
+final class DefaultNewsManager: MainNewsDataProvider {
     private let apiService = APIService()
     private let dbService = DBDataLoader()
     private var newsFromApi: [News] = []
@@ -54,20 +54,21 @@ final class DefaultNewsManager: NewsManager {
         dbService.saveData(newsFromBD)
     }
     
-    func updateFavorites(with news: News, currentFavoriteState: Bool, refreshCell: @escaping () -> ()) {
+    func updateFavorites(with news: News, currentFavoriteState: Bool, completion: (Actions) -> ()) {
         if currentFavoriteState {
             guard let indexOfEqual = newsFromBD.firstIndex(of: news) else { return }
             newsFromBD.remove(at: indexOfEqual)
+            completion(.delete)
         }
         else {
             news.isFavorite = !currentFavoriteState
             newsFromBD.append(news)
+            completion(.refresh)
         }
-        refreshCell()
     }
 }
 
-extension DefaultNewsManager: FavoriteNewsManager {
+extension DefaultNewsManager: FavoriteNewsDataProvider {
     func loadFavoriteNews() {
         delegate?.modelDidLoadNews(newsFromBD)
     }
